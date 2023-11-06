07cf5c3f352658017954bb05a442645be9f57f40
malek chiha <malekchiha75@gmail.com>
2023-11-06 21:53:42 +0100

    edit3477999gghfffff

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO activity_sector (id_secteur_activite, code_secteur_activite, libelle_secteur_activite)
-VALUES (555,'D134563D','hellooo1'),(222,'D1345116D','hellooo2'),(333,'D13234456D','hellooo3');
+VALUES (222,'D1345116D','hellooo2'),(333,'D13234456D','hellooo3');
