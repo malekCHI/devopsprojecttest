5f90d431dcbca7bd8bbe2e5ea53174e801f3b147
malek chiha <malekchiha75@gmail.com>
2023-11-06 18:52:31 +0100

    edit569

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO activity_sector (id_secteur_activite, code_secteur_activite, libelle_secteur_activite)
-VALUES (11,'D13456D','hellooo'),(12,'D13456D','hellooo'),(13,'D13456D','hellooo');
+VALUES (111,'D13456D','hellooo'),(222,'D13456D','hellooo'),(333,'D13456D','hellooo');
