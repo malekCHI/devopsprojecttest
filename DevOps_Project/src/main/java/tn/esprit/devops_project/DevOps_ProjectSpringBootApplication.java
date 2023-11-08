14e711813e069b54ddeeff8a9570f964b00a66f0
malek chiha <malekchiha75@gmail.com>
2023-11-08 12:24:23 +0100

    lastttttt nchalhh

--- DevOps_Project/src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
@@ -2,10 +2,8 @@ package tn.esprit.devops_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "tn.esprit")
 public class DevOps_ProjectSpringBootApplication {
 
     public static void main(String[] args) {
